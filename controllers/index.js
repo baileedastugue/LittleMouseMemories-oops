955d7b42d607415dc9079d90ec9846859368d3e7
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-11 14:24:55 -0500

    requires a title for the album to be created

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
+const User = db.User;
 
 // Landing page
 router.get("/", (req, res) => {
@@ -16,6 +17,28 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
     res.render("dashboard", {
         user: req.user
     });
+})
+
+router.post("/dashboard/album", ensureAuthenticated, (req, res) => {
+    console.log(req.user);
+    console.log("create album");
+    console.log(req.body);
+    const title = req.body.title;
+    let errors = [];
+    if (!title) {
+        console.log("erro");
+        errors.push({
+            msg: "Please enter a title for your album"
+        });
+    }
+    if (errors.length > 0) {
+        res.render("dashboard", {
+            errors, 
+            user: req.user 
+        })
+    } else {
+        console.log("good job at title");
+    }
 })
 
 module.exports = router;
