9894cc77ed15295b1b4d1aae57d21f733dccf281
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-11 12:50:46 -0500

    successfully logs user out

--- controllers/users.js
+++ controllers/users.js
@@ -110,4 +110,13 @@ router.post("/login", (req, res, next) => {
       })(req, res, next);
 })
 
+// logout handler
+router.get("/logout", (req, res) => {
+    req.logout();
+    req.flash(
+        "success_msg", 
+        "You are logged out");
+    res.redirect("/users/login");
+})
+
 module.exports = router;
