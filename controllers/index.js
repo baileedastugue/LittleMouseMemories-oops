47dcdd926b4ea2d4c6705462a7134b852a3a5595
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-11 15:41:45 -0500

    dashboard is populated with album names and updates when a new one is made

--- controllers/index.js
+++ controllers/index.js
@@ -14,34 +14,48 @@ router.get("/", (req, res) => {
 
 // Dashboard
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-    console.log(req.user);
-    res.render("dashboard", {
-        user: req.user
-    });
+    // console.log(req.user);
+    
+    User.findOne({
+        _id: req.user._id
+    })
+        .populate("album")
+        .then(user => {
+            console.log("populated album successful");
+            console.log(user.album[0]);
+            res.render("dashboard", {
+                user: user
+            });
+        })
+        .catch(err => console.log(err));
 })
 
 router.post("/dashboard/album", ensureAuthenticated, (req, res) => {
-    console.log(req.user);
-    console.log("create album");
-    console.log(req.body);
+    // console.log(req.user);
+    // console.log("create album");
+    // console.log(req.body);
     const title = req.body.title;
     let errors = [];
     if (!title) {
-        console.log("erro");
+        // send an error if the user does not add a title
         errors.push({
             msg: "Please enter a title for your album"
         });
     }
     if (errors.length > 0) {
+        // if there is an error, send it to the page
         res.render("dashboard", {
             errors, 
             user: req.user 
         })
     } else {
-        console.log("good job at title");
+        // if the user actually enters a title,
+        // this checks to see if that album name already exists
         Album.findOne({ title: title })
             .then(album => {
                 if(album) {
+                    // if there is an album with that name
+                    // push an error
                     errors.push({
                         msg: "Please enter a unique album title"
                     })
@@ -50,24 +64,30 @@ router.post("/dashboard/album", ensureAuthenticated, (req, res) => {
                         user: req.user
                     })
                 } else {
-                    console.log("this is a unique album title");
+                    // if the album title is unique
                     Album.create({ title: title })
+                        // that album is created
                         .then(album => {
+                            // if the album is successfully created,
+                            // update the user's album array so they are associated
                             return User.findOneAndUpdate({
                                 _id: req.user._id
                             }, { $push: { album: album._id} }, 
                             { new: true });
                         })
                         .then(album => {
+                            console.log("line 69");
+                            // if the album is successfully associated,
+                            // send a success message
+                            
+                            res.redirect("/dashboard");
                             req.flash(
                                 "success_msg",
                                 "Album created"
                             );
-                            console.log("hello from line 66");
-                            console.log(album);
-                            console.log("album created")
-                            res.send("you did it");
+                            console.log(req.user);
                         }) 
+                        .catch(err => console.log(err));
                             
                 }
             })
