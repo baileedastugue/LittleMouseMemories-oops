53a5cf06c823ff5977fa735c21793db1532c69fc
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-15 15:26:36 -0500

    can find all albums for a specific user

--- models/Album.js
+++ models/Album.js
@@ -1,31 +1,34 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AlbumsSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    // password: {
-    //     type: String
-    // },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    pictures: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Pictures"
-        }
-    ],
-    prompt: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Prompt"
-        }
-    ]
+     title: {
+          type: String,
+          required: true,
+     },
+     // password: {
+     //     type: String
+     // },
+     date: {
+          type: Date,
+          default: Date.now,
+     },
+     user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'user',
+     },
+     pictures: [
+          {
+               type: Schema.Types.ObjectId,
+               ref: 'Pictures',
+          },
+     ],
+     prompt: [
+          {
+               type: Schema.Types.ObjectId,
+               ref: 'Prompt',
+          },
+     ],
 });
 
-
-module.exports = Album = mongoose.model("Album", AlbumsSchema);
+module.exports = Album = mongoose.model('Album', AlbumsSchema);
--- routes/api/albums.js
+++ routes/api/albums.js
@@ -1,77 +1,81 @@
 const db = require('../../models');
 const Album = db.Album;
+const User = db.User;
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 // Dashboard
 
 // @route   GET api/albums
-// @desc    Get all albums
-// @access  Public
-router.get('/', (req, res) => {
-    Album.find() 
-        .sort({ date: -1 })
-        .then(albums => res.json(albums))
-    
-    // User.findOne({
-    //     _id: req.user._id
-    // })
-    //     .populate("album")
-    //     .then(user => {
-    //         // console.log("populated album successful");
-    //         // console.log(user.album[0]);
-    //         res.render("dashboard", {
-    //             user: user
-    //         });
-    //     })
-    //     .catch(err => console.log(err));
-})
+// @desc    Get current user's albums
+// @access  Private
+router.get('/', auth, async (req, res) => {
+     try {
+          console.log(req.user.id);
+          const album = await Album.find({
+               user: req.user.id,
+          }).populate('User', ['firstName']);
+
+          if (!album) {
+               res.status(400).json({
+                    msg: 'There are no albums for this user',
+               });
+          }
+
+          res.json(album);
+     } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server error');
+     }
+});
 
 // @route   POST api/albums
 // @desc    Post a new album
-// @access  Public
-router.post('/', (req, res) => {
-    const newAlbum = new Album({
-        title: req.body.title
-    });
+// @access  Private
+router.post(
+     '/',
+     [auth, [check('title', 'A title is required').not().isEmpty()]],
+     async (req, res) => {
+          const errors = validationResult(req);
+          if (!errors.isEmpty()) {
+               return res.status(400).json({ errors: errors.array() });
+          }
+          const albumFields = {};
+          const { title } = req.body;
+          albumFields.title = title;
+          albumFields.user = req.user.id;
+          console.log(albumFields);
 
-    newAlbum.save()
-        .then(album => res.json(album));
-})
+          try {
+               let album = Album.find({ user: req.user.id });
+               // create album
+               album = new Album(albumFields);
+               await album.save();
+               res.json(album);
+          } catch (err) {
+               console.error(err.message);
+               res.status(500).send('Server Error');
+          }
+     }
+);
 
 // @route   DELETE api/albums/:id
 // @desc    Delete an album
 // @access  Public
 router.delete('/:id', (req, res) => {
-    Album.findById(req.params.id) 
-        .then(album => album.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false }));
+     Album.findById(req.params.id)
+          .then((album) =>
+               album.remove().then(() => res.json({ success: true }))
+          )
+          .catch((err) => res.status(404).json({ success: false }));
 });
-
 
 module.exports = router;
 
-
 // // router.post("/dashboard/album", ensureAuthenticated, (req, res) => {
-// //     // console.log(req.user);
-// //     // console.log("create album");
-// //     // console.log(req.body);
-// //     const title = req.body.title;
-// //     let errors = [];
-// //     if (!title) {
-// //         // send an error if the user does not add a title
-// //         errors.push({
-// //             msg: "Please enter a title for your album"
-// //         });
-// //     }
-// //     if (errors.length > 0) {
-// //         // if there is an error, send it to the page
-// //         res.render("dashboard", {
-// //             errors, 
-// //             user: req.user 
-// //         })
-// //     } else {
+
 // //         // if the user actually enters a title,
 // //         // this checks to see if that album name already exists
 // //         Album.findOne({ title: title })
@@ -95,7 +99,7 @@ module.exports = router;
 // //                             // update the user's album array so they are associated
 // //                             return User.findOneAndUpdate({
 // //                                 _id: req.user._id
-// //                             }, { $push: { album: album._id} }, 
+// //                             }, { $push: { album: album._id} },
 // //                             { new: true });
 // //                         })
 // //                         .then(album => {
@@ -107,8 +111,8 @@ module.exports = router;
 // //                                 "Album created"
 // //                             );
 // //                             res.redirect("/dashboard");
-                            
-// //                         }) 
+
+// //                         })
 // //                         .catch(err => console.log(err));
 // //                 }
 // //             })
@@ -120,7 +124,7 @@ module.exports = router;
 // //         album: req.params.id
 // //     });
 // //     console.log(req.params.id);
-    
+
 // // })
 
 // // router.post("/albums/:id/pictures", (req, res) => {
@@ -146,7 +150,7 @@ module.exports = router;
 // //                         .then(picture => {
 // //                             // return Album.findOneAndUpdate({
 // //                             //     _id: req.body.id
-// //                             // }, { $push: { pictures: picture } }, 
+// //                             // }, { $push: { pictures: picture } },
 // //                             // { new: true });
 // //                             console.log(picture);
 // //                         })
