b9955fdbeaba86767c8a9348b481c8b277bdd294
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-16 21:09:22 -0500

    invalid credentials show up on login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LoginForm from '../components/LoginForm/index';
 import { Container } from 'reactstrap';
+import AlertDiv from '../components/Alert/index';
 
 class Login extends Component {
      render() {
           return (
                <Container>
+                    <AlertDiv />
                     <h5>Returning User</h5>
                     <LoginForm />
                     {/* <%- include('./partials/messages.ejs') %> */}
