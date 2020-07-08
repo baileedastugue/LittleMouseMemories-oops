fe9a92c7081223b840bce10ad125e2db88cefb84
Bailee Dastugue <bailee.dast@gmail.com>
2020-07-08 09:03:55 -0500

    reconfigured store

--- client/src/store.js
+++ client/src/store.js
@@ -2,20 +2,22 @@
 // holds the whole state tree of your application
 
 import { createStore, applyMiddleware, compose } from 'redux';
+// import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/';
 
-// const initialState = {};
+const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+// const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(
      rootReducer,
-     /* preloadedState, */ composeEnhancers(
-          applyMiddleware(...middleware),
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-               window.__REDUX_DEVTOOLS_EXTENSION__()
+     initialState,
+     compose(
+          // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          // window.__REDUX_DEVTOOLS_EXTENSION__(),
+          applyMiddleware(...middleware)
      )
 );
 
