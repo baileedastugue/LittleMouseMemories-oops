090f60b75be7a5d4a8f723f2e0a67c262962b6d1
Bailee Dastugue <bailee.dast@gmail.com>
2020-09-30 08:02:40 -0500

    changed welcome forms positioning

--- client/src/components/Layout/AboutSection/index.js
+++ client/src/components/Layout/AboutSection/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
+import { Container, Row } from 'reactstrap';
 import OpenCloseForm from '../OpenCloseForm';
 
 const AboutSection = () => {
      return (
-          <div className='about'>
-               <div className='about-background'></div>
-               <div className='about-info'>
-                    <div>
-                         <h2 className='about-info--primary'>
-                              Memories worth sharing,
-                         </h2>
-
-                         <h2 className='about-info--primary'>
-                              privacy worth keeping
-                         </h2>
-                    </div>
-                    <div className='about-info--secondary'>
-                         Post photos and write your own memories,
-                         <br />
-                         easily collaborate with friends and family,
-                         <br />
-                         and protect your albums with a password
-                         <br />
-                    </div>
-                    <OpenCloseForm />
-               </div>
-          </div>
+          <>
+               <Container fluid={true} className='about'>
+                    <div className='about-background'></div>
+                    <Container fluid={true} className='about-info'>
+                         <Row>
+                              <h2 className='about-info--primary'>
+                                   Memories worth sharing,
+                              </h2>
+                         </Row>
+                         <Row>
+                              <h2 className='about-info--primary'>
+                                   privacy worth keeping
+                              </h2>
+                         </Row>
+                         <Row className='about-info--secondary'>
+                              Post photos and write your own memories,
+                              <br />
+                              easily collaborate with friends and family,
+                              <br />
+                              and protect your albums with a password
+                              <br />
+                         </Row>
+                    </Container>
+               </Container>
+               <OpenCloseForm />
+          </>
      );
 };
 
